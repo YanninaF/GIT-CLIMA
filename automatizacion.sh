7aa97e5a815598f65daec41359d7384344768c91
Yannina Fariña <yannifar24@gmail.com>
2024-09-18 16:02:19 -0400

    Agregado la automatizacion.sh y el requirements

---
+++ automatizacion.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 1. Activar el entorno virtual
+echo "Activando el entorno virtual..."
+source .venv/Scripts/activate
+
+# 2. Instalar dependencias
+echo "Instalando dependencias..."
+pip install -r requirements.txt 
+
+# 3. Ejecutar la aplicación CLI
+echo "Ejecutando la aplicacion CLI..."
+python api_clima.py --formato json asuncion
+
+# 4. Mensaje final
+echo "Ejecución completada con éxito."
+
+#./automatizacion.sh
